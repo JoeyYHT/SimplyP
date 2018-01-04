47e2a501f945a3ea7420e91d5ca463ba32a0e6d5
James Sample <james.sample@niva.no>
2018-01-04 13:28:08 +0100

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -29,4 +29,5 @@ setup(name='simply_p',
                         'pandas',
                         'seaborn',
                         'numpy',
-                        'scipy'])
+                        'scipy',
+                        'xlrd'])
