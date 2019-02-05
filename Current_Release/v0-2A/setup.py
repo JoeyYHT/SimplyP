eeb94dda1de63e317c30e108e890a8db8545e3b9
James Sample <james.e.sample@gmail.com>
2019-02-05 16:10:51 +0100

    Simplify setup.py

--- Current_Release/v0-2A/setup.py
+++ Current_Release/v0-2A/setup.py
@@ -4,18 +4,14 @@
 from setuptools import setup
 from codecs import open
 
-# Get the long description from the README file
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(name='simplyP',
       version='0.2',
-      description='A parsimonious, semi-distributed and dynamic phosphorus model, implemented in Python.',
-      long_description=long_description,
-      url='https://github.com/LeahJB/SimplyP',
+      packages=['simplyP',],
       author='Leah Jackson-Blake',
       author_email='leah.jackson-blake@niva.no',
-      license='MIT',
+      url='https://github.com/NIVANorge/niva_datasci_toolkit',
+      description='A parsimonious, semi-distributed and dynamic phosphorus model, implemented in Python.',
+      long_description=open('../../README.md').read(),
       classifiers=['Development Status :: 3 - Alpha',
                    'License :: OSI Approved :: MIT License',
                    'Intended Audience :: Developers',
@@ -26,7 +22,6 @@ setup(name='simplyP',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6'],
       keywords='phosphorus modelling water quality',
-      py_modules=['simplyP'],
       install_requires=['matplotlib==2.0.2',
                         'pandas==0.22.0',
                         'seaborn==0.8.1',
